0982dbb56a49c796cb2064db04fbb1a5b0b850cd
kay fujita <gain820120@yahoo.co.jp>
2021-06-26 12:31:51 +0000

    second fix

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(num)
     if num % 15 == 0
          "FizzBuzz"
-    elsif num  % 5 == 0
+    elsif num % 5 == 0
          "Buzz"
-    elsif num  % 3 == 0
+    elsif num % 3 == 0
          "Fizz"
     else
          num
@@ -13,5 +13,3 @@ num_max = 100
 (1..num_max).each do |num|
     puts fizzbuzz(num)
 end
-
-
