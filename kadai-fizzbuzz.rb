331736090eaaebfa1ec145ca7338e5989cc7dc6f
kay fujita <gain820120@yahoo.co.jp>
2021-06-26 11:51:42 +0000

    first fix

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,17 +1,17 @@
 def fizzbuzz(num)
     if num % 15 == 0
-        puts "FizzBuzz"
+         "FizzBuzz"
     elsif num  % 5 == 0
-        puts "Buzz"
+         "Buzz"
     elsif num  % 3 == 0
-        puts "Fizz"
+         "Fizz"
     else
-        puts num
+         num
     end
 end
 num_max = 100
-    (1..num_max).each do|num|
-   fizzbuzz(num)
+(1..num_max).each do |num|
+    puts fizzbuzz(num)
 end
 
-"fizzbuzz"
+
